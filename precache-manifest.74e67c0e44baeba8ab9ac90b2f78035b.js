fdf205ad94da256e44a1c6b7767c2643847ee83b
yesworld <a.yesworld@gmail.com>
2019-05-16 17:53:15 +0300

    Updates

---
+++ precache-manifest.74e67c0e44baeba8ab9ac90b2f78035b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d65f50a3986f6d56ddf9be6c1f8c54",
+    "url": "/portfolio/img/layer_6.58d65f50.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  },
+  {
+    "revision": "6dac52ee2397a0aef8d7bee3ca54a8d3",
+    "url": "/portfolio/img/layer_4.6dac52ee.png"
+  },
+  {
+    "revision": "8f624e6be4a416308932",
+    "url": "/portfolio/css/chunk-69cd9326.7592e1a8.css"
+  },
+  {
+    "revision": "e53a6c4be498a21e8185",
+    "url": "/portfolio/css/chunk-vendors.f79d5b11.css"
+  },
+  {
+    "revision": "e53a6c4be498a21e8185",
+    "url": "/portfolio/js/chunk-vendors.9df23649.js"
+  },
+  {
+    "revision": "3fba6171457e44f057a63c1776ba23a8",
+    "url": "/portfolio/img/layer_3.3fba6171.png"
+  },
+  {
+    "revision": "e64c723ba01137ed51b5",
+    "url": "/portfolio/js/app.47af14e9.js"
+  },
+  {
+    "revision": "8f624e6be4a416308932",
+    "url": "/portfolio/js/chunk-69cd9326.2b378a88.js"
+  },
+  {
+    "revision": "94949ef8cf7c2ac3726c3bdf00d6be53",
+    "url": "/portfolio/img/layer_1.94949ef8.png"
+  },
+  {
+    "revision": "d4b1f6b37b1ec924590632317f7b8629",
+    "url": "/portfolio/img/layer_2.d4b1f6b3.png"
+  },
+  {
+    "revision": "89b9b17c712658fe8ae0d911e80a8626",
+    "url": "/portfolio/img/layer_5.89b9b17c.png"
+  },
+  {
+    "revision": "f8c89ed1d56765c5af85a143dba2b0db",
+    "url": "/portfolio/img/avatar.f8c89ed1.jpeg"
+  },
+  {
+    "revision": "9f98937ebdfa0b47c05538faaf95cfd6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e64c723ba01137ed51b5",
+    "url": "/portfolio/css/app.bebe19af.css"
+  }
+];
